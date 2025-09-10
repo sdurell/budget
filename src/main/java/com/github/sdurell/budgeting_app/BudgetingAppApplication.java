cabec2d1327dcdfdd0f3c5df3bcaf93422ecc1b5
sdurell <124106192+sdurell@users.noreply.github.com>
2025-09-10 14:44:45 -0400

    Revert "Learning SpringBoot tutorial"
    This reverts commit 51eb57dba7f65418c2a8a8a68e9ed9280c71423c.

---
+++ src/main/java/com/github/sdurell/budgeting_app/BudgetingAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.sdurell.budgeting_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetingAppApplication.class, args);
+	}
+
+}
