cc1931acfde3595faa35aa46f1ca9c09e5565219
sdurell <spencerdurell@gmail.com>
2025-09-11 17:55:12 -0400

    Create frontend structure

---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,31 @@
+import MovieCard from "../components/MovieCard";
+import { useMovieContext } from "../contexts/MovieContext";
+import "../css/Favorites.css";
+
+function Favorites() {
+    const { favorites } = useMovieContext();
+
+    if (favorites) {
+        return (
+            <div>
+                <h2>Your Favorites</h2>
+                <div className="movies-grid">
+                    {favorites.map(
+                        (movie) =>
+                        (
+                            <MovieCard movie={movie} ley={movie.id} />
+                        )
+                    )}
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <div className="favorites-empty">
+            <h2>No Favorite Movies Yet </h2>
+            <p>Start adding movies to your favorites and they will appear here.</p>
+        </div>
+    )
+}
+export default Favorites
